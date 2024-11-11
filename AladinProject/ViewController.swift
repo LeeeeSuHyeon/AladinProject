1735829c278e07391e48fcfc7a97b3103d5ba92f
suhyeon lee <tngus0673@naver.com>
2024-11-11 17:39:23 +0900

    gitignore 파일 추가

---
+++ AladinProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AladinProject
+//
+//  Created by 이수현 on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
