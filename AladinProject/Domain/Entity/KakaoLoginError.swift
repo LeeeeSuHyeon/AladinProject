643590b5c2662b949529ee4c536a0f5713750ffe
suhyeon lee <tngus0673@naver.com>
2024-11-25 17:51:25 +0900

    Kakao Token 유효성 검사 후 로그인 구현

--- AladinProject/Domain/Entity/KakaoLoginError.swift
+++ AladinProject/Domain/Entity/KakaoLoginError.swift
@@ -11,6 +11,8 @@ public enum KakaoLoginError : Error {
     case loginUnavailable
     case loginFailed
     case tokenError
+    case invalidTokenError // 현재 저장된 토큰이 유효하지 않은 경우
+    case tokenOtherError // 토큰은 유효하나 다른 에러 (앱 에러, 인증 및 인가 에러, API 에러 등)
     
     var description : String {
         switch self {
@@ -20,6 +22,10 @@ public enum KakaoLoginError : Error {
             "카카오 로그인에 실패했습니다."
         case .tokenError :
             "토큰이 없습니다."
+        case .invalidTokenError:
+            "토큰이 유효하지 않습니다."
+        case .tokenOtherError:
+            "토큰은 유효하나 인증, API 등 기타 에러"
         }
     
     }
--- AladinProject/Presentation/ViewModel/LoginViewModel.swift
+++ AladinProject/Presentation/ViewModel/LoginViewModel.swift
@@ -46,25 +46,64 @@ public class LoginViewModel : LoginViewModelProtocol{
     
     
     private func kakaoLogin() {
-        UserApi.shared.loginWithKakaoAccount { oauthToken, error in
-            if let error = error {
-                print("loginWithKakaoAccount - \(error)")
-                self.error.accept(KakaoLoginError.loginFailed.description)
-            } else {
-                // 토큰 저장
-                guard let token = oauthToken else {
-                    print("token Error")
-                    self.error.accept(KakaoLoginError.tokenError.description)
-                    return
-                }
-                let status = KeychainService.shared.save(account: .my, service: .kakaoLogin, value: token.accessToken)
-                if status == errSecSuccess {
-                    print("KakaoLoginToken - Saved")
-                    self.isLoginSuccess.accept(true)
+        
+        self.hasKakaoToken {[weak self] result in
+            switch result {
+            case .success(let hasToken):
+                if hasToken {
+                    print("Token 유효")
+                    self?.isLoginSuccess.accept(true)
                 } else {
-                    // 키 체인 에러
+                    UserApi.shared.loginWithKakaoAccount { oauthToken, error in
+                        if let error = error {
+                            print("loginWithKakaoAccount - \(error)")
+                            self?.error.accept(KakaoLoginError.loginFailed.description)
+                        } else {
+                            // 토큰 저장
+                            guard let token = oauthToken else {
+                                print("token Error")
+                                self?.error.accept(KakaoLoginError.tokenError.description)
+                                return
+                            }
+                            let status = KeychainService.shared.save(account: .my, service: .kakaoLogin, value: token.accessToken)
+                            if status == errSecSuccess {
+                                print("KakaoLoginToken - Saved")
+                                self?.isLoginSuccess.accept(true)
+                            } else {
+                                // 키 체인 에러
+                            }
+                        }
+                    }
+                }
+            case .failure(let error):
+                self?.error.accept(error.description)
+            }
+        }
+    }
+    
+    private func hasKakaoToken(completion: @escaping (Result<Bool, KakaoLoginError>) -> Void) {
+        if (AuthApi.hasToken()) { // 토큰이 있다면
+            UserApi.shared.accessTokenInfo { ( _ , error) in // 토큰 정보 불러오기
+                if let error = error {
+                    if let sdkError = error as? SdkError, sdkError.isInvalidTokenError() == true  {
+                        // 토큰이 유효하지 않은 경우
+                        //로그인 필요
+                        completion(.success(false))
+                    }
+                    else {
+                        //기타 에러
+                        completion(.failure(.tokenOtherError))
+                    }
+                }
+                else {
+                    //토큰 유효성 체크 성공(필요 시 토큰 갱신됨)
+                    completion(.success(true))
                 }
             }
+        }
+        else { // 토큰이 없다면
+            //로그인 필요
+            completion(.success(false))
         }
     }
 }
